d475d5a7e1585fe60b7c6c3fce1a12c369eefdc3
Jonathan Binns <jonny.binns1@gmail.com>
2019-10-17 10:42:42 +0100

    got 8.4 working, it was a typo you idiot

--- chapter8/loggingapp.py
+++ chapter8/loggingapp.py
@@ -9,13 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     this_route = url_for('.root')
-    app.logger.info("logging a test message from "+this_route)
+    app.logger.info("logging a test message from "+ this_route)
     return "Hello Napier from the config testing app, now with added logging"
 
 def init(app):
     config = ConfigParser.ConfigParser()
     try: 
         config_location = "etc/logging.cfg"
+        config.read(config_location)
         app.config['DEBUG'] = config.get("config", "debug")
         app.config['ip_address'] = config.get("config", "ip_Address")
         app.config['port'] = config.get("config", "port")
@@ -30,7 +31,7 @@ def logs(app):
     log_pathname = app.config['log_location'] + app.config['log_file']
     file_handler = RotatingFileHandler(log_pathname, maxBytes=1024*1024*10, backupCount=1024)
     file_handler.setLevel( app.config['log_level'] )
-    formatter = logging.Formatter("%(levelname)s | %(asctime)s | %(module)s | %(funcNames)s | %(message)s")
+    formatter = logging.Formatter("%(levelname)s | %(asctime)s | %(module)s | %(funcName)s | %(message)s")
     file_handler.setFormatter(formatter)
     app.logger.setLevel( app.config['log_level'] )
     app.logger.addHandler(file_handler)
