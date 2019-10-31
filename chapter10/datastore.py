2414590525049ded6d4846a5cfaee654345e99d9
Jonathan Binns <jonny.binns1@gmail.com>
2019-10-31 15:01:12 +0000

    aded some of chapter 11

--- chapter10/datastore.py
+++ chapter10/datastore.py
@@ -15,10 +15,10 @@ def get_db():
 @app.teardown_appcontext
 def close_db_connection(exception):
     db = getattr(g, 'db', None)
-    if db is not None: 
+    if db is not None:
         db.close()
 
-def init_db(): 
+def init_db():
     with app.app_context():
         db = get_db()
         with app.open_resource('schema.sql', mode='r') as f:
@@ -28,16 +28,16 @@ def init_db():
 @app.route("/")
 def root():
     db = get_db()
-    db.cursor().execute('insert into albums values ("American Beauty", "Grateful Dead", "CD")')
+    db.cursor().execute('insert into albums values "American Beauty", "Grateful Dead", "CD")')
     db.commit()
 
     page = []
     page.append('<html><ul>')
     sql = "SELECT rowid, * FROM albums ORDER BY artist"
-    for row in db.curor().execute(sql):
+    for row in db.cursor().execute(sql):
         page.append('<li>')
         page.append(str(row))
-        page.append('</li>')
+        page.append('<li>')
 
     page.append('</ul></html>')
     return ''.join(page)
